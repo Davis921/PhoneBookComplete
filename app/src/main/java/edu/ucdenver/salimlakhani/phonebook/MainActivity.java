588fc8bc96114504b25336c501e20305074d669d
Salim Lakhani <salim.lakhani@ucdenver.edu>
2024-04-01 13:02:52 -0600

    Added code for Recycler

--- app/src/main/java/edu/ucdenver/salimlakhani/phonebook/MainActivity.java
+++ app/src/main/java/edu/ucdenver/salimlakhani/phonebook/MainActivity.java
@@ -1,14 +1,59 @@
 package edu.ucdenver.salimlakhani.phonebook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import edu.ucdenver.salimlakhani.phonebook.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private ArrayList<Contact> list;
+
+    private ContactAdapter contactAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        list = new ArrayList<Contact>();
+        contactAdapter = new ContactAdapter(this, list);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        binding.content.recyclerView.setLayoutManager(layoutManager);
+        binding.content.recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        binding.content.recyclerView.setAdapter(contactAdapter);
+
+
+
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AddContactDialog addContactDialog = new AddContactDialog();
+                addContactDialog.show(getSupportFragmentManager(), "");
+            }
+        }
+
+        );
+    } // End of onCreate method
+
+    public void addContact (Contact contact) {
+        list.add(contact);
+        contactAdapter.notifyDataSetChanged();
+
+        //Log.i ("info", "Number of Contacts" + list.size());
     }
-}
+} //End of class
